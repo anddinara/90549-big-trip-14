8d2310d46c1fd0e186e970b7feb127ba05fcfb78
anddinara <and.dinara.moscow@gmail.com>
2021-04-10 21:01:37 +0300

    feat: add modules

--- src/main.js
+++ src/main.js
@@ -1,12 +1,40 @@
+import {createTripInfoTemplate} from './view/trip-info.js';
+import {createCostTripTemplate} from './view/cost.js';
+import {createSiteMenuTemplate} from './view/site-menu.js';
+import {createFilterTemplate} from './view/filter.js';
+import {createSortTemplate} from './view/sort.js';
+import {createTripEventsListTemplate} from './view/trip-events-list.js';
+import {createFormEditTemplate} from './view/form-edit.js';
+import {createFormAddTemplate} from './view/form-add.js';
+import {createTripElementTemplate} from './view/trip-element.js';
+
+const TRIP_ELEMENT = 3;
+
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
+const siteHeaderElement = document.querySelector('.page-header');
+const tripMainElement = siteHeaderElement.querySelector('.trip-main');
+const siteMenuElement = siteHeaderElement.querySelector('.trip-controls__navigation');
+const siteFilterElement = siteHeaderElement.querySelector('.trip-controls__filters');
+const siteMainElement = document.querySelector('.page-main');
+const tripEventsElement = siteMainElement.querySelector('.trip-events');
 
-// 4. Создайте модули (отдельные файлы) и опишите в них
-// функции для генерации DOM-элементов. Эти модули — наши будущие компоненты,
-// view в рамках MVP. Поэтому:
+render(tripMainElement, createTripInfoTemplate(), 'afterbegin');
 
-// -для них нужно завести отдельную директорию src/view;
-// -именуйте файлы как существительные (да, мы пока осознанно нарушаем критерий Б27);
-// -для экспорта функций используйте именованный экспорт.
+const tripInfoElement = siteHeaderElement.querySelector('.trip-info');
+render(tripInfoElement, createCostTripTemplate(), 'beforeend');
+
+render(siteMenuElement, createSiteMenuTemplate(), 'beforeend');
+render(siteFilterElement, createFilterTemplate(), 'beforeend');
+render(tripEventsElement, createSortTemplate(), 'beforeend');
+render(tripEventsElement, createTripEventsListTemplate(), 'beforeend');
+
+const tripEventsListElement = siteMainElement.querySelector('.trip-events__list');
+render(tripEventsListElement, createFormEditTemplate(), 'beforeend');
+render(tripEventsListElement, createFormAddTemplate(), 'beforeend');
+
+for (let i = 0; i < TRIP_ELEMENT; i++) {
+  render(tripEventsListElement, createTripElementTemplate(), 'beforeend');
+}
